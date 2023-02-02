3175fef32ea736a34948b006b343feacf1615c1b
dennisyaj <dennispilco@gmail.com>
2023-02-02 13:00:39 -0500

    tarea 11

---
+++ src/main/java/com/example/demo/ProyectoPwU3DpTarea11Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPwU3DpTarea11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPwU3DpTarea11Application.class, args);
+	}
+
+}
